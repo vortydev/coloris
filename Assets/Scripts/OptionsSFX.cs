f06961683326c16e187cbd03be1cec06320f253c
vortydev <etienne.dmenard@gmail.com>
2021-03-02 23:22:51 -0500

    SFX options toggles now play their associated sound

---
+++ Assets/Scripts/OptionsSFX.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSFX : MonoBehaviour
+{
+    [SerializeField] AudioSource sfxSource;
+    [SerializeField] AudioClip move;
+    [SerializeField] AudioClip rotate;
+    [SerializeField] AudioClip hardDrop;
+    [SerializeField] AudioClip hold;
+
+    private void PlaySFX(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        sfxSource.Play();
+    }
+
+    public void PlaySfxMove()
+    {
+        PlaySFX(move);
+    }
+
+    public void PlaySfxRotate()
+    {
+        PlaySFX(rotate);
+    }
+
+    public void PlaySfxHardDrop()
+    {
+        PlaySFX(hardDrop);
+    }
+
+    public void PlaySfxHold()
+    {
+        PlaySFX(hold);
+    }
+}
